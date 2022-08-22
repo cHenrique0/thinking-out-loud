919a04f604325af8846bfd050a89b84b2fb6f600
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-22 11:51:36 -0300

    Settings for database (postgres) connection

---
+++ src/database/config/connection.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
+  seederStorage: "sequelize",
+  seederStorageTableName: "SequelizeDataSeed",
+  define: {
+    underscored: true,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
