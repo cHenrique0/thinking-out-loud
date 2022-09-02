e1a288a57176fd604467f1f8249bcab66364e725
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-02 13:08:33 -0300

    Adding preview image on user profile edit page

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,5 +1,4 @@
 // Disabling form submissions if there are invalid fields
-
 const forms = document.querySelectorAll(".needs-validation");
 // Loop over them and prevent submission
 forms.forEach((form) => {
@@ -15,4 +14,17 @@ forms.forEach((form) => {
     },
     false
   );
+});
+
+// Preview user profile image
+const inputImg = document.querySelector("#profile-image");
+
+inputImg.addEventListener("change", (event) => {
+  const target = event.target;
+  const files = target.files;
+  const fileReader = new FileReader();
+  fileReader.onload = () => {
+    document.querySelector("#preview-image").src = fileReader.result;
+  };
+  fileReader.readAsDataURL(files[0]);
 });
