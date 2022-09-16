3d400132093a04630798e9e9a1e657ed86d10808
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-16 13:45:13 -0300

    Changing the main layout

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,5 +1,15 @@
-// Disabling form submissions if there are invalid fields
+/* Selecting the page components */
+const html = document.documentElement;
+const body = document.body;
+const toUpBtn = document.querySelector("#to-up-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navList = document.querySelector(".nav-mobile");
+const navLinks = document.querySelectorAll(".nav-mobile .nav-item");
 const forms = document.querySelectorAll(".needs-validation");
+const inputImg = document.querySelector("#profile-picture");
+const textArea = document.querySelector("#title");
+
+// Disabling form submissions if there are invalid fields
 if (forms) {
   // Loop over them and prevent submission
   forms.forEach((form) => {
@@ -19,7 +29,6 @@ if (forms) {
 }
 
 // Preview user profile picture
-const inputImg = document.querySelector("#profile-picture");
 if (inputImg) {
   inputImg.addEventListener("change", (event) => {
     const target = event.target;
@@ -37,7 +46,6 @@ if (inputImg) {
 }
 
 // Limiting characters in the text area
-const textArea = document.querySelector("#title");
 if (textArea) {
   const charQty = document.querySelector("#chars");
   const limit = textArea.maxLength;
@@ -73,3 +81,45 @@ emojiButtons.forEach((btn) => {
     console.log(textArea);
   });
 }); */
+
+/* Detecting page scroll down */
+const scrollFunction = () => {
+  if (body.scrollTop > 50 || html.scrollTop > 50) {
+    /* Showing the Go To Up button if scroll down */
+    // toUpBtn.style.visibility = "visible";
+    /* Hiding the navbar if scroll down */
+    if (navList.classList.contains("active")) {
+      toggleMobileMenu();
+    }
+    return;
+  }
+  /* Hiding go to up button if in the header */
+  // toUpBtn.style.visibility = "hidden";
+  return;
+};
+/* Assigning the window scroll function */
+window.onscroll = () => scrollFunction();
+
+/* mobile navbar animation */
+const animateLinks = () => {
+  /* Selecting all links in the navbar menu */
+  navLinks.forEach((link, index) => {
+    let time = (index / 7) * 0.8;
+    /* Setting up the animation function for each link */
+    link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `navLinksFade 0.5s ease forwards ${time}s`);
+  });
+};
+
+/* Open/Close the menu */
+const toggleMobileMenu = (event) => {
+  animateLinks();
+  navList.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+};
+/* Checking if the mobile menu is displaying */
+if (mobileMenu) {
+  /* Adding a click event to it */
+  mobileMenu.addEventListener("click", toggleMobileMenu);
+}
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const IndexController = require("../controllers/IndexController");
+const { checkUserLogged } = require("../middlewares/loginMiddlewares");
 
 const indexRouter = Router();
 
-indexRouter.get("/home", IndexController.getHomePage);
+indexRouter.get("/home", checkUserLogged, IndexController.getHomePage);
 
-indexRouter.get("/", IndexController.redirectToHomePage);
+indexRouter.get("/", checkUserLogged, IndexController.redirectToHomePage);
 
 module.exports = indexRouter;
